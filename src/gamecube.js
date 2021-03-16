f092234a02ded72d85be7250dcaf2706284d5ed1
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-03-16 18:08:01 -0500

    rewrote the gamecube class to handle new cubie class

--- src/gamecube.js
+++ src/gamecube.js
@@ -1,26 +1,57 @@
 class Gamecube {
-    constructor(n, block_size) {
-        this.n = n;
-        this.block_size = block_size;
-        this.gameboard = this.initGameboard(n);
+    constructor() {
+        this.gameboard = this.initGameboard();
+        this.cubiesMoving = false;
     }
 
-    initGameboard(n) {
-        let gb = new Array(n);
-        for (let x = 0; x < n; x++) {
-            gb[x] = new Array(n);
-            for (let y = 0; y < n; y++) {
-                gb[x][y] = new Array(n);
-                for (let z = 0; z < n; z++) {
-                    gb[x][y][z] = 0;
+    initGameboard() {
+        let gb = new Array(N);
+        for (let x = 0; x < N; x++) {
+            gb[x] = new Array(N);
+            for (let y = 0; y < N; y++) {
+                gb[x][y] = new Array(N);
+                for (let z = 0; z < N; z++) {
+                    gb[x][y][z] = null;
                 }
             }
         }
         return gb;
     }
 
+    isGameboardFull() {
+        for (let x = 0; x < this.gameboard.length; x++) {
+            for (let y = 0; y < this.gameboard[x].length; y++) {
+                for (let z = 0; z < this.gameboard[x][y].length; z++) {
+                    if (this.gameboard[x][y][z] === null) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    addRandomCubie() {
+        let randX;
+        let randY;
+        let randZ;
+        let randValue;
+        while (!this.isGameboardFull()) {
+            randX = floor(random(N));
+            randY = floor(random(N));
+            randZ = floor(random(N));
+            randValue = floor(random(1, 3));
+
+            if (this.gameboard[randX][randY][randZ] === null) {
+                this.gameboard[randX][randY][randZ] = new Cubie(randX, randY, randZ, randValue);
+                return true;
+            }
+        }
+        return false;
+    }
+
     render() {
-        const cube_width = this.block_size * this.n;
+        const cube_width = BLOCK_SIZE * N;
 
         // render outside box
         strokeWeight(4);
@@ -28,147 +59,72 @@ class Gamecube {
         noFill();
         box(cube_width + 2);
 
+        const vertex_locations = cube_width / 2 + 4;
+
+        // drawing visual aid for front of cube
+        beginShape();
+        strokeWeight(8);
+        stroke(255, 0, 0);
+        vertex(vertex_locations, vertex_locations, vertex_locations);
+        vertex(-vertex_locations, vertex_locations, vertex_locations);
+        vertex(-vertex_locations, -vertex_locations, vertex_locations);
+        vertex(vertex_locations, -vertex_locations, vertex_locations);
+        endShape(CLOSE);
+
         // render cubies from gameboard matrix
         for (let x = 0; x < this.gameboard.length; x++) {
             for (let y = 0; y < this.gameboard[x].length; y++) {
                 for (let z = 0; z < this.gameboard[x][y].length; z++) {
-                    if (this.gameboard[x][y][z] != 0) {
-                        this.renderCubie(x, y, z, this.gameboard[x][y][z]);
+                    if (this.gameboard[x][y][z] !== null) {
+                        this.gameboard[x][y][z].render();
                     }
                 }
             }
         }
-
     }
 
-    renderCubie(x, y, z, value) {
-        push();
-
-        const offset = -(this.n / 2 * this.block_size) + this.block_size / 2;
-        translate(offset, offset, offset);
-
-        translate(x * this.block_size, y * this.block_size, z * this.block_size);
-
-        strokeWeight(1);
-        stroke(0);
-
-        fill(255, 102, 94);
-        box(this.block_size);
-
-        pop();
-    }
-
-    shiftCubiesUp() {
-        for (let y = 0; y < this.gameboard[0].length; y++) {
-            for (let x = 0; x < this.gameboard.length; x++) {
-                for (let z = 0; z < this.gameboard[x][y].length; z++) {
-                    // if the cubie is not already at the top of the cube and there is not a cubie already above it
-                    if (y != 0 && this.gameboard[x][y][z] > 0) {
-                        if (this.gameboard[x][y - 1][z] == 0) {
-                            let temp = this.gameboard[x][y][z];
-                            this.gameboard[x][y][z] = 0;
-                            this.gameboard[x][y - 1][z] = temp;
-                        }
-                    }
-
-                    // NEED TO IMPLEMET CUBIE MERGING
-                }
-            }
-        }
-    }
-
-    shiftCubiesDown() {
-        for (let y = this.n - 1; y >= 0; y--) {
-            for (let x = 0; x < this.gameboard.length; x++) {
-                for (let z = 0; z < this.gameboard[x][y].length; z++) {
-                    // if the cubie is not already at the bottom of the cube and there is not a cubie already below it
-                    if (y != this.n - 1 && this.gameboard[x][y][z] > 0) {
-                        if (this.gameboard[x][y + 1][z] == 0) {
-                            let temp = this.gameboard[x][y][z];
-                            this.gameboard[x][y][z] = 0;
-                            this.gameboard[x][y + 1][z] = temp;
-                        }
-                    }
-
-                    // NEED TO IMPLEMET CUBIE MERGING
-                }
-            }
-        }
-    }
-
-    shiftCubiesWest() {
+    currentlyAnimating() {
         for (let x = 0; x < this.gameboard.length; x++) {
             for (let y = 0; y < this.gameboard[x].length; y++) {
                 for (let z = 0; z < this.gameboard[x][y].length; z++) {
-                    // if the cubie is not already to the left of the cube and there is not a cubie already to the left of it
-                    if (x != 0 && this.gameboard[x][y][z] > 0) {
-                        if (this.gameboard[x - 1][y][z] == 0) {
-                            let temp = this.gameboard[x][y][z];
-                            this.gameboard[x][y][z] = 0;
-                            this.gameboard[x - 1][y][z] = temp;
+                    if (this.gameboard[x][y][z] !== null) {
+                        if (this.gameboard[x][y][z].isMoving) {
+                            return true;
                         }
                     }
-
-                    // NEED TO IMPLEMET CUBIE MERGING
                 }
             }
+        }
+        return false;
+    }
+
+    moveCubie(x1, y1, z1, x2, y2, z2) {
+        if (x1 < 0 || x1 >= N) return false;
+        if (x2 < 0 || x2 >= N) return false;
+        if (y1 < 0 || y1 >= N) return false;
+        if (y2 < 0 || y2 >= N) return false;
+        if (z1 < 0 || z1 >= N) return false;
+        if (z2 < 0 || z2 >= N) return false;
+
+        if (this.gameboard[x1][y1][z1] !== null) {
+            if (this.gameboard[x2][y2][z2] === null) {
+
+                this.gameboard[x2][y2][z2] = this.gameboard[x1][y1][z1].moveTo(x2, y2, z2);
+                this.gameboard[x1][y1][z1] = null;
+
+                return true;
+            } else if (this.gameboard[x2][y2][z2].value === this.gameboard[x1][y1][z1].value) {
+                // MERGE
+                return false;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
         }
     }
 
-    shiftCubiesEast() {
-        for (let x = this.n - 1; x >= 0; x--) {
-            for (let y = 0; y < this.gameboard[x].length; y++) {
-                for (let z = 0; z < this.gameboard[x][y].length; z++) {
-                    // if the cubie is not already to the right of the cube and there is not a cubie already to the right of it
-                    if (x != this.n - 1 && this.gameboard[x][y][z] > 0) {
-                        if (this.gameboard[x + 1][y][z] == 0) {
-                            let temp = this.gameboard[x][y][z];
-                            this.gameboard[x][y][z] = 0;
-                            this.gameboard[x + 1][y][z] = temp;
-                        }
-                    }
+    shiftCubiesUp() {
 
-                    // NEED TO IMPLEMET CUBIE MERGING
-                }
-            }
-        }
-    }
-
-    shiftCubiesNorth() {
-        for (let z = 0; z < this.gameboard[0][0].length; z++) {
-            for (let x = 0; x < this.gameboard.length; x++) {
-                for (let y = 0; y < this.gameboard[x].length; y++) {
-                    // if the cubie is not already to the north of the cube and there is not a cubie already to the north of it
-                    if (z != 0 && this.gameboard[x][y][z] > 0) {
-                        if (this.gameboard[x][y][z - 1] == 0) {
-                            let temp = this.gameboard[x][y][z];
-                            this.gameboard[x][y][z] = 0;
-                            this.gameboard[x][y][z - 1] = temp;
-                        }
-                    }
-
-                    // NEED TO IMPLEMET CUBIE MERGING
-                }
-            }
-        }
-    }
-
-    shiftCubiesSouth() {
-        for (let z = this.n - 1; z >= 0; z--) {
-            for (let x = 0; x < this.gameboard.length; x++) {
-                for (let y = 0; y < this.gameboard[x].length; y++) {
-                    // if the cubie is not already to the south of the cube and there is not a cubie already to the south of it
-                    if (z != this.n - 1 && this.gameboard[x][y][z] > 0) {
-                        if (this.gameboard[x][y][z + 1] == 0) {
-                            let temp = this.gameboard[x][y][z];
-                            this.gameboard[x][y][z] = 0;
-                            this.gameboard[x][y][z + 1] = temp;
-                        }
-                    }
-
-                    // NEED TO IMPLEMET CUBIE MERGING
-                }
-            }
-        }
     }
 }
