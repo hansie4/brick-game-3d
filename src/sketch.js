bd7014e6ccf511c1b1e30287b5944654d93ba477
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-03-17 14:31:06 -0500

    added basic functionality of the game

--- src/sketch.js
+++ src/sketch.js
@@ -1,14 +1,11 @@
-const VIEWPORT_WIDTH = 700;
-const VIEWPORT_HEIGHT = 700;
+const VIEWPORT_WIDTH = 600;
+const VIEWPORT_HEIGHT = 600;
 
-const BLOCK_SIZE = 60;
+const BLOCK_SIZE = 50;
 const N = 4;
 
+let scoreElement;
 let gamecube;
-
-let x = 0;
-let y = 0;
-let z = 0;
 
 function setup() {
   createCanvas(VIEWPORT_WIDTH, VIEWPORT_HEIGHT, WEBGL);
@@ -16,20 +13,13 @@ function setup() {
 
   frameRate(30);
 
-  gamecube = new Gamecube(N, BLOCK_SIZE);
-
-  //gamecube.gameboard[floor(random(N))][floor(random(N))][floor(random(N))] = 2;
-
-  gamecube.gameboard[2][3][0] = 2;
-  gamecube.gameboard[1][3][0] = 2;
-  gamecube.gameboard[3][3][1] = 2;
-  gamecube.gameboard[1][3][1] = 2;
-
-  gamecube.gameboard[1][1][1] = 2;
-
-  console.log(gamecube.gameboard);
-
   camera();
+
+  gamecube = new Gamecube();
+
+  scoreElement = select("#gameScore");
+
+  scoreElement.html('Score: ' + gamecube.score);
 }
 
 function draw() {
@@ -37,34 +27,7 @@ function draw() {
 
   orbitControl();
 
-  //gamecube.gameboard[x][y][z] = 2;
-
   gamecube.render();
-
-  // if (frameCount % 15 == 0) {
-  //   x++;
-  //   if (x >= N) {
-  //     x = 0;
-  //     y++;
-  //   }
-  //   if (y >= N) {
-  //     y = 0;
-  //     z++;
-  //   }
-  //   if (z >= N) {
-  //     x = 0;
-  //     y = 0;
-  //     z = 0;
-  //     for (let i = 0; i < N; i++) {
-  //       for (let j = 0; j < N; j++) {
-  //         for (let k = 0; k < N; k++) {
-  //           gamecube.gameboard[i][j][k] = 0;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
 }
 
 function keyPressed() {
@@ -73,12 +36,20 @@ function keyPressed() {
   } else if (keyCode === 83) {
     gamecube.shiftCubiesSouth();
   } else if (keyCode === 65) {
-    gamecube.shiftCubiesWest();
-  } else if (keyCode === 68) {
     gamecube.shiftCubiesEast();
+  } else if (keyCode === 68) {
+    gamecube.shiftCubiesWest();
   } else if (keyCode === 81) {
     gamecube.shiftCubiesUp();
   } else if (keyCode === 69) {
     gamecube.shiftCubiesDown();
+  } else if (keyCode === 13) {
+    gamecube.addRandomCubie();
+  } else if (keyCode === 16) {
+    console.log(gamecube.gameboard);
   }
+
+  gamecube.addRandomCubie();
+  gamecube.calculateScore();
+  scoreElement.html('Score: ' + gamecube.score);
 }
